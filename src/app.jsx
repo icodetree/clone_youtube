213b9c6f366f5e0f2623463fd151e2974cc20315
codetree <codetree@codetreeui-MacBookPro.local>
2022-04-04 17:15:13 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,26 @@
-import './app.css';
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return (
-    <div className="App">
-      Hello react!!
-    </div>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDelGoqmhxIXr6UfTS8vPQ8Pptrz0nu7cs",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
