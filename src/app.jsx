cf95e6dc31696fc8331f383f5eff5dcf3309a545
icodetree <icodetree@naver.com>
2022-08-31 16:45:13 +0900

    youtube search

--- src/app.jsx
+++ src/app.jsx
@@ -1,29 +1,51 @@
 import React, { useEffect, useState } from "react";
-import "./app.css";
+import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
 import SearchBar from "./components/header/search_bar";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
-  const [videos, setVideos] = useState([]);
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedvideo] = useState(null);
 
-  const search = (query) => {
-    youtube
-      .search(query) //
-      .then((videos) => setVideos(videos));
-  };
+    const selectVideo = (video) => {
+        setSelectedvideo(video);
+    };
+    const search = (query) => {
+        setSelectedvideo(null);
+        youtube
+            .search(query) //
+            .then((videos) => {
+                setVideos(videos);
+            });
+    };
 
-  useEffect(() => {
-    youtube
-      .mostPopular() //
-      .then((videos) => setVideos(videos));
-  }, []);
+    useEffect(() => {
+        youtube
+            .mostPopular() //
+            .then((videos) => setVideos(videos));
+    }, []);
 
-  return (
-    <div>
-      <SearchBar onSearch={search} />
-      <VideoList videos={videos} />;
-    </div>
-  );
+    return (
+        <div className={styles.app}>
+            <SearchBar onSearch={search} />
+            <section className={styles.content}>
+                {selectedVideo && (
+                    <div className={styles.detail}>
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                )}
+                <div className={styles.list}>
+                    <VideoList
+                        videos={videos}
+                        onVideoClick={selectVideo}
+                        display={selectedVideo ? "list" : "grid"}
+                    />
+                    ;
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
